696af0fdeaf5d8bc0c94c25c6b4aed26c2666cc3
Rafael Luz <rafaellladm@gmail.com>
2021-04-13 19:47:18 -0300

    definition of the user class

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,26 @@
 import { v4 as uuidV4 } from "uuid";
 
 class User {
-  // Complete aqui
+  id?: string;
+
+  name: string;
+
+  admin: boolean;
+
+  email: string;
+
+  create_at: Date;
+
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+
+    this.admin = false;
+    this.updated_at = undefined;
+  }
 }
 
 export { User };
